6079198232cb4f8aeab077f9828438d33f86f3ef
Zulfiqar Tamin <fiqartamin@gmail.com>
2019-09-04 15:09:40 +0700

    mempercantik tampilan

--- app/src/main/java/com/fiqartamin/moviecatalogue/DetailActivity.java
+++ app/src/main/java/com/fiqartamin/moviecatalogue/DetailActivity.java
@@ -2,11 +2,13 @@ package com.fiqartamin.moviecatalogue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_MOVIE = "extra_movie";
-    TextView tvTitle;
+    TextView tvTitle, tvRelease, tvDesc;
+    ImageView imgPhoto, imgThumb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,18 @@ public class DetailActivity extends AppCompatActivity {
         if (selectedMovie != null) {
             tvTitle = findViewById(R.id.movie_det_title);
             tvTitle.setText(selectedMovie.getTitle());
+
+            tvRelease = findViewById(R.id.movie_det_release);
+            tvRelease.setText(selectedMovie.getRelease());
+
+            tvDesc = findViewById(R.id.movie_det_desc);
+            tvDesc.setText(selectedMovie.getDesc());
+
+            imgPhoto = findViewById(R.id.movie_det_img);
+            imgPhoto.setImageResource(selectedMovie.getPhoto());
+
+            imgThumb = findViewById(R.id.movie_det_thumb);
+            imgThumb.setImageResource(selectedMovie.getPhoto());
 
             if(getSupportActionBar() != null){
                 getSupportActionBar().setTitle(selectedMovie.getTitle());
